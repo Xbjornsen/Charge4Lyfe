2b4c28449a7bcd7885acdd9e31316052e0bb061a
XBjornsen <xaviertr7@gmail.com>
2018-10-06 13:56:10 +0930

    JS and css improvements
    Added comments to the js file so that its easier to understand
    
    changed some csss attributes to make it all look simpler

--- www/js/js.js
+++ www/js/js.js
@@ -1,3 +1,4 @@
+//javascript to retrieve "map" id and then insert innerhmtl elements
 document.getElementById("map").innerHTML = `
 <header>Charge4lyfe</header>
     <div class="container">
@@ -5,6 +6,7 @@ document.getElementById("map").innerHTML = `
         <h1><button onclick="mapFunction()">Find location on Map</button></h1>
     </div>
 `
+//js to display the list of states and link to go back home
 function stateFunction() {
     document.getElementById("map").innerHTML =`
 <header>Charge4Lyfe</header>
@@ -23,15 +25,17 @@ function stateFunction() {
 </div>
 <h1><a href="index.html">Home</a></h1>
 `
+//js to add and event listener to change the background when the mouse hovers over the target button. Timeout function is very short at 50 milliseconds
 var hover = document.getElementById("hover");
    hover.addEventListener("mouseover", function(event){
        event.target.style.background ="grey";
        setTimeout(function(){
            event.target.style.background ="";
-       }, 500);
+       }, 50);
    }, false);
 }
 
+//function to display the map_header and Home link within the body tag
 function ntFunction() {
     document.getElementById("body").innerHTML =  `
 <div class="map_header">
@@ -39,9 +43,9 @@ function ntFunction() {
 </div>
 <div id="map"></div>
 <h2><a href="index.html">Home</a></h2>
-<h2><button onclick="stateFunction()">Back to Select State</button</h2>
-`
 
+`
+//function to initialize the map and fetch markers from a Json file. The markers are then plotted on the map.
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: -12.37, lng: 130.87},
@@ -55,7 +59,7 @@ var bounds;
     
 
 function plotMarkers(m)
-{
+{  
   markers = [];
   bounds = new google.maps.LatLngBounds();
 
@@ -90,6 +94,7 @@ bounds.extend(position);
 initMap();
 }
 
+//function to execute find location on map button. This will render the map with users location
 function mapFunction(){
     document.getElementById("body").innerHTML=`
 <div class="map_header">
@@ -97,7 +102,7 @@ function mapFunction(){
 </div>
 <div id="map"></div>
 <h2><a href="index.html">Home</a></h2>
-<h2><button onclick="stateFunction()">Back to Select State</button</h2>
+
 `
 var map;
 function initMap() {
