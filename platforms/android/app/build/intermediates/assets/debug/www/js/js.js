e15ec85109ce1d9012e5d1e9579a927c720d1b6e
XBjornsen <xaviertr7@gmail.com>
2018-10-08 12:06:59 +0930

    Adjusted screen ratio
    Adjusted width and height.
    The was an issue when i rotated my device and the app didn't adjust to the extra width. it should be fixed now

--- platforms/android/app/build/intermediates/assets/debug/www/js/js.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/js.js
@@ -1,12 +1,18 @@
-document.getElementById("map").innerHTML = `
+//javascript to retrieve "map" id and then insert innerhmtl elements
+document.getElementById("main").innerHTML = `
 <header>Charge4lyfe</header>
     <div class="container">
         <h1><button onclick="stateFunction()">Select State</button></h1>
         <h1><button onclick="mapFunction()">Find location on Map</button></h1>
     </div>
 `
+document.getElementById("about").innerHTML= `
+<div class="info">
+    <h1><button onclick="infoFunction()">About</button></h1>
+`
+//js to display the list of states and link to go back home
 function stateFunction() {
-    document.getElementById("map").innerHTML =`
+    document.getElementById("main").innerHTML =`
 <header>Charge4Lyfe</header>
  <h3>Select State/Territory</h3>
 <div id="hover" class="list">
@@ -23,15 +29,20 @@ function stateFunction() {
 </div>
 <h1><a href="index.html">Home</a></h1>
 `
+//removed element img
+var el = document.getElementById('img1');
+    el.parentNode.removeChild(el);
+//js to add and event listener to change the background when the mouse hovers over the target button. Timeout function is very short at 50 milliseconds
 var hover = document.getElementById("hover");
    hover.addEventListener("mouseover", function(event){
        event.target.style.background ="grey";
        setTimeout(function(){
            event.target.style.background ="";
-       }, 500);
+       }, 50);
    }, false);
 }
 
+//function to display the map_header and Home link within the body tag
 function ntFunction() {
     document.getElementById("body").innerHTML =  `
 <div class="map_header">
@@ -39,9 +50,9 @@ function ntFunction() {
 </div>
 <div id="map"></div>
 <h2><a href="index.html">Home</a></h2>
-<h2><button onclick="stateFunction()">Back to Select State</button</h2>
-`
 
+`
+//function to initialize the map and fetch markers from a Json file. The markers are then plotted on the map.
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: -12.37, lng: 130.87},
@@ -55,7 +66,7 @@ var bounds;
     
 
 function plotMarkers(m)
-{
+{  
   markers = [];
   bounds = new google.maps.LatLngBounds();
 
@@ -90,6 +101,7 @@ bounds.extend(position);
 initMap();
 }
 
+//function to execute find location on map button. This will render the map with users location
 function mapFunction(){
     document.getElementById("body").innerHTML=`
 <div class="map_header">
@@ -97,7 +109,7 @@ function mapFunction(){
 </div>
 <div id="map"></div>
 <h2><a href="index.html">Home</a></h2>
-<h2><button onclick="stateFunction()">Back to Select State</button</h2>
+
 `
 var map;
 function initMap() {
